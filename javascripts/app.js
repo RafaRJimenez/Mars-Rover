a522a87d609ad99511d019e87f300df755e54ad5
fernando mora romero <fernandomorar88@gmail.com>
2018-03-06 16:01:31 +0100

    Mars' Rover second change

--- javascripts/app.js
+++ javascripts/app.js
@@ -4,3 +4,63 @@ var rover = {
     y: 0,
     travelLog: [],
     };
+
+function TurnLeft(rover){
+	switch(rover.direction){
+		case 'N':
+			rover.direction='W';
+			return ('Rover is now facing west');
+			break;
+		case 'W':
+			rover.direction='S';
+			return ('Rover is now facing south');
+			break;
+		case 'S':
+			rover.direction='E';
+			return ('Rover is now facing east');
+			break;
+		default :
+			rover.direction='N';
+			return ('Rover is now facing north');
+	}
+}
+
+
+function TurnRight(rover){
+	switch(rover.direction){
+		case 'N':
+			rover.direction='E';
+			return ('Rover is now facing east');
+			break;
+		case 'E':
+			rover.direction='S';
+			return ('Rover is now facing south');
+			break;
+		case 'S':
+			rover.direction='W';
+			return ('Rover is now facing west');
+			break;
+		default :
+			rover.direction='N';
+			return ('Rover is now facing north');
+	}
+}
+
+
+function moveFordward(rover){
+	if(rover.direction='N'){
+		rover.y= rover.y - 1;
+	}
+	else if(rover.direction='E'){
+		rover.x= rover.x + 1;
+	}
+	else if(rover.direction='S'){
+		rover.y= rover.y + 1;
+	}
+	else {
+		rover.x= rover.x - 1;
+	}
+	console.log (rover.x + ',' + rover.y);
+	
+}
+
